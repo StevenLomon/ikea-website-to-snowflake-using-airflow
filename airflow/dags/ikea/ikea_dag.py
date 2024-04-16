57e2b53711d7349f5a300f740321faee4bb890d5
Steven Lomon Lennartsson <steven.lennartsson@gmail.com>
2024-04-16 14:24:47 +0000

    transform_ikea_data task done

--- airflow/dags/ikea/ikea_dag.py
+++ airflow/dags/ikea/ikea_dag.py
@@ -1,7 +1,12 @@
+import boto3
+import pandas as pd
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import timedelta, datetime
-from ikea_etl import extract_raw_data
+from ikea_etl import extract_raw_data, transform_raw_ikea_data
+
+s3_client = boto3.client('s3')
+target_bucket_name = 'ikea-airflow-bucket'
 
 url = "https://www.ikea.com/se/sv/new/new-products/"
 
@@ -15,6 +20,22 @@ def extract_data(**kwargs):
     df.to_csv(f"{file_str}.csv", index=False)
     output_file_path = f"/home/ubuntu/{file_str}.csv"
     return [output_file_path, file_str]
+
+def transform_data(task_instance):
+    data = task_instance.xcom_pull(task_ids='tsk_extract_ikea_data')[0]
+    object_key = task_instance.xcom_pull(task_ids='tsk_extract_ikea_data')[1]
+    df = pd.read_csv(data)
+
+    clean_ikea_data = transform_raw_ikea_data(df)
+    print('Num of rows:', len(df))
+    print('Num of cols:', len(df.columns))
+
+    # Convert DataFrame to CSV format
+    csv_data = clean_ikea_data.to_csv(index=False)
+
+    # Upload CSV to S3
+    object_key = f"{object_key}.csv"
+    s3_client.put_object(Bucket=target_bucket_name, Key=object_key, Body=csv_data)
 
 default_args = {
     'owner': 'airflow',
@@ -36,4 +57,9 @@ with DAG('ikea_dag',
         task_id= 'tsk_extract_ikea_data',
         python_callable=extract_data,
         op_kwargs={'url': url}
+    )
+
+    transform_ikea_data = PythonOperator(
+        task_id= 'tsk_transform_ikea_data',
+        python_callable=transform_data,
     )
--- airflow/dags/ikea/ikea_etl.py
+++ airflow/dags/ikea/ikea_etl.py
@@ -23,12 +23,17 @@ def extract_raw_data(url) -> pd.DataFrame:
     return df
 
 df_ikea = extract_raw_data(ikea_url)
-df_ikea['Color name'] = df_ikea['colors'].apply(lambda x: x[0].get('name') if x and 'name' in x[0] else None)
-df_ikea['Color hex'] = df_ikea['colors'].apply(lambda x: x[0].get('hex') if x and 'hex' in x[0] else None)
 
-df_ikea = df_ikea.loc[:,['pipUrl', 'id', 'name', 'typeName', 'mainImageUrl', 'ratingValue', 'ratingCount', 'salesPrice.current.wholeNumber', 'Color name', 'Color hex', 'mainImageAlt']]
-df_ikea.rename(columns = {'pipUrl':'URL', 'id':'ID', 'name':'Name', 'typeName':'Type', 'mainImageUrl':'Image URL', 'ratingValue':'Rating value',
-                        'ratingCount':'Rating count', 'salesPrice.current.wholeNumber':'Price', 'mainImageAlt':'Description'}, inplace=True)
-df_ikea.reset_index(drop=True, inplace=True)
+def transform_raw_ikea_data(raw_data_df:pd.DataFrame) -> pd.DataFrame:
+    raw_data_df['Color name'] = raw_data_df['colors'].apply(lambda x: x[0].get('name') if x and 'name' in x[0] else None)
+    raw_data_df['Color hex'] = raw_data_df['colors'].apply(lambda x: x[0].get('hex') if x and 'hex' in x[0] else None)
 
-df_ikea.to_csv(result_name)
+    raw_data_df = raw_data_df.loc[:,['pipUrl', 'id', 'name', 'typeName', 'mainImageUrl', 'ratingValue', 'ratingCount', 'salesPrice.current.wholeNumber', 'Color name', 'Color hex', 'mainImageAlt']]
+    raw_data_df.rename(columns = {'pipUrl':'URL', 'id':'ID', 'name':'Name', 'typeName':'Type', 'mainImageUrl':'Image URL', 'ratingValue':'Rating value',
+                            'ratingCount':'Rating count', 'salesPrice.current.wholeNumber':'Price', 'mainImageAlt':'Description'}, inplace=True)
+    clean_data_df = raw_data_df.reset_index(drop=True)
+
+    return clean_data_df
+
+df_ikea_clean = transform_raw_ikea_data(df_ikea)
+df_ikea_clean.to_csv(result_name)
